3e2e62318986c05131e116ed87f140efde06ea2d
vladislav99dev <vladislavcho99@abv.bg>
2022-11-29 16:29:59 +0200

    add wrapper div to services

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -11,31 +11,33 @@ import { SocialMediaBtnIcon } from "../../assets/images/servicesIcons/servicesIc
 
 const Services = () => {
   return (
-    <aside className={styles.services}>
-      <h2 className={styles.heading}>Services</h2>
+    <aside className={styles.aside}>
+      <div className={styles.container}>
+        <h2 className={styles.heading}>Services</h2>
 
-      {servicesData.services.map((service) => {
-        return (
-          <div key={service.id} className={styles.serviceContainer}>
-            <h2 className={styles.secondaryHeading}>{service.title}</h2>
-            <p className={styles.description}>{service.description}</p>
-            <button className={styles.btn}>
-              View more
-              <div className={styles.btnLogoContainer}>
-                {service.title === "Design" ? (
-                  <DesignBtnIcon />
-                ) : service.title === "Development" ? (
-                  <DevelopmentBtnIcon />
-                ) : service.title === "E-commerce" ? (
-                  <ECommerceBtnIcon />
-                ) : (
-                  <SocialMediaBtnIcon />
-                )}
-              </div>
-            </button>
-          </div>
-        );
-      })}
+        {servicesData.services.map((service) => {
+          return (
+            <div key={service.id} className={styles.serviceContainer}>
+              <h2 className={styles.serviceHeading}>{service.title}</h2>
+              <p className={styles.serviceDescription}>{service.description}</p>
+              <button className={styles.btn}>
+                View more
+                <div className={styles.btnLogoContainer}>
+                  {service.title === "Design" ? (
+                    <DesignBtnIcon />
+                  ) : service.title === "Development" ? (
+                    <DevelopmentBtnIcon />
+                  ) : service.title === "E-commerce" ? (
+                    <ECommerceBtnIcon />
+                  ) : (
+                    <SocialMediaBtnIcon />
+                  )}
+                </div>
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </aside>
   );
 };
