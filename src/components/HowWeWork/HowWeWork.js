8ca9705261d2d8cfd4292f1fc27c40c0da1f4680
vladislav99dev <vladislavcho99@abv.bg>
2022-11-29 14:07:29 +0200

    fix HowWeWork styling

--- src/components/HowWeWork/HowWeWork.js
+++ src/components/HowWeWork/HowWeWork.js
@@ -1,29 +1,34 @@
 import React from "react";
 
 import howWeWorkData from "../../assets/data/howWeWork.json";
-import Button from "../Button/Button"
+import Button from "../Button/Button";
 
 import * as styles from "./HowWeWork.module.css";
 
-const HowWeWork = ({toggleForm}) => {
+const HowWeWork = ({ toggleForm }) => {
   return (
     <section className={styles.container}>
       <h2 className={styles.heading}>How we work</h2>
       <div className={styles.stepsContainer}>
         {howWeWorkData.howWeWork.map((item, index) => {
           return (
-            <div key={item.id} className={styles.stepContainer}>
-              <h3 className={styles.stepHeading}>{item.title}</h3>
+            // <>
+              <div key={item.id} className={styles.stepContainer}>
+                <h3 className={styles.stepHeading}>{item.title}</h3>
                 <p className={styles.stepNumber}>{index + 1}</p>
-              <p className={styles.stepDescription}>
-                {item.description}
-              </p>
+                <p className={styles.stepDescription}>{item.description}</p>
               <div className={styles.underline}></div>
-            </div>
+              </div>
+            // </>
           );
         })}
       </div>
-        <Button width={'24rem'} height={'4.5rem'} fontSize={'1.8rem'} toggleForm={toggleForm}/>
+      <Button
+        width={"30rem"}
+        height={"5rem"}
+        fontSize={"1.8rem"}
+        toggleForm={toggleForm}
+      />
     </section>
   );
 };
