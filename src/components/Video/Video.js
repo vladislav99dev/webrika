dcca859bb9ec8eeed7f1bb0e2e20234a87cd82f5
vladislav99dev <vladislavcho99@abv.bg>
2022-11-29 12:00:31 +0200

    add a container div to Video and pass and display children

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-const Video = ({video}) => {
+const Video = ({video,children}) => {
   return (
+    <div className='relative' >
     <video className="w-full h-full object-fill" autoplay="autoplay" muted loop>
       <source  src={video} type="video/mp4"/>
     </video>
+      {children}
+    </div>
   );
 };
 
