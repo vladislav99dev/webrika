e6228a53826dd1386835e9c2416b64026ebaca22
vladislav99dev <vladislavcho99@abv.bg>
2022-11-29 10:45:47 +0200

    pass the video to Video in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import HowWeWork from "../components/HowWeWork/HowWeWork";
 import Video from "../components/Video/Video";
 import ServicesNProducts from "../components/ServicesNProducts/ServicesNProducts";
 import ContactUs from "../components/ContactUs/ContactUs";
-
+import WeAreWebrikaVideo from "../assets/video/WeAreWebrika.mp4"
 import "../styles/global.css";
 
 const IndexPage = () => {
@@ -14,7 +14,7 @@ const IndexPage = () => {
   return (
     <>
       <HowWeWork toggleForm={toggleForm} />
-      <Video />
+      <Video video={WeAreWebrikaVideo} />
       <ServicesNProducts />
       <ContactUs toggleForm={toggleForm} />
     </>
