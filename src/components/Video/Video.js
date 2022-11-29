9ea8c97f7b75e79288b2eada98bb0ddc9b92bf17
vladislav99dev <vladislavcho99@abv.bg>
2022-11-29 09:32:55 +0200

    import video as component and pass it to video tag

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,24 +1,11 @@
 import React from "react";
-
-import {MonitorImage} from "../../assets/images/monitorImage"
-
-import * as styles from "./Video.module.css";
+import WeAreWebrikaVideo from "../../assets/video/WeAreWebrika.mp4"
 
 const Video = () => {
   return (
-    <section className={styles.container}>
-      <p className={styles.placeholder}>Video</p>
-      <div className={styles.mobileContent}>
-
-      <h2 className={styles.heading}>Lorem ipsum ipsum dolor</h2>
-      <p className={styles.description}>
-        Lorem ipsum dolor sit amet, sectetur adipiscing elit,In frungila arcu
-        lacina sodales porta. Lorem ipsum dolor sit amet, sectetur adispiscing
-        elit. in frigila arcu lacinia sodales porta
-      </p>
-      <MonitorImage className={styles.monitorImage}/>
-      </div>
-    </section>
+    <video src={WeAreWebrikaVideo}  autoplay="autoplay" muted loop>
+      <source  src={WeAreWebrikaVideo} type="video/mp4"/>
+    </video>
   );
 };
 
