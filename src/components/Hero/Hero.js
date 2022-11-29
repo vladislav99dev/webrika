8b9cf6cb1cdbf449a2519a84f82ed5b05cca753a
vladislav99dev <vladislavcho99@abv.bg>
2022-11-29 12:04:51 +0200

    make Hero work with Video component and fix some styles

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { WebrikaTextBig } from "../../assets/images/logos/logos";
+import HeroVideo from "../../assets/video/heroVideo.mp4";
 
 import Navbar from "../Navbar/Navbar";
 import Services from "../Services/Services";
@@ -8,26 +9,28 @@ import Button from "../Button/Button";
 
 import * as styles from "./Hero.module.css";
 
+import Video from "../Video/Video";
+
 const Hero = ({ toggleForm }) => {
+  const HeroText = (
+    <div className={styles.textContainer}>
+      <WebrikaTextBig />
+      <h3 className={styles.textHeading}>Digital agency</h3>
+      <p className={styles.textDescription}>
+        Let's work together to make your <br /> business idea a reality
+      </p>
+      <Button
+        width={"30rem"}
+        height={"5rem"}
+        fontSize={"1.8rem"}
+        toggleForm={toggleForm}
+      />
+    </div>
+  );
+
   return (
     <section className={styles.container}>
-      <div className={styles.hero}>
-        <Navbar toggleForm={toggleForm} />
-
-        <div className={styles.textContainer}>
-          <WebrikaTextBig className={styles.textLogo} />
-          <h3 className={styles.textHeading}>Digital agency</h3>
-          <p className={styles.textDescription}>
-            Let's work together to make your <br /> business idea a reality
-          </p>
-          <Button
-            width={"24rem"}
-            height={"4.5rem"}
-            fontSize={"1.8rem"}
-            toggleForm={toggleForm}
-          />
-        </div>
-      </div>
+      <Video video={HeroVideo}>{<Navbar/>}{HeroText}</Video>
       <Services />
     </section>
   );
