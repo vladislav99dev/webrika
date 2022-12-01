1ee9a779fc64dab29e2b7354dbc477d7b7e5e699
vladislav99dev <vladislavcho99@abv.bg>
2022-12-01 12:08:40 +0200

    make css responsive to Mobile Screens in ContactUs

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,29 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import Button from "../Button/Button"
+import Button from "../Button/Button";
 
-import * as styles from "./ContactUs.module.css"
+import * as styles from "./ContactUs.module.css";
 
-const ContactUs = ({toggleForm}) => {
+const ContactUs = ({ toggleForm }) => {
   return (
-    <section id='contact' className={styles.section}>
-        <h2 className={styles.heading}>Contact us</h2>
-        <p className={styles.pInformation}>Contact information</p>
-        <div className={styles.contactsContainer}>
-            <div className={styles.labels}>
-                <p>Contact person:</p>
-                <p>Contact number:</p>
-                <p>Contact email:</p>
-            </div>
-            <div className={styles.information}>
-                <p>George Prusiyski</p>
-                <p>+359 87 873 5795</p>
-                <p>g.prusiyski@webrika.bg</p>
-            </div>
-        </div>
-          <Button width={'44rem'} height={'5rem'} fontSize={'2.2rem'}  toggleForm={toggleForm}/>
+    <section id="contact" className={styles.section}>
+      <h2 className={styles.heading}>Contact us</h2>
+      <p className={styles.pInformation}>Contact information</p>
+      <div className={styles.contactsContainer}>
+        <p className={styles.labels}>Contact person:</p>
+        <p className={styles.information}>George Prusiyski</p>
+        <p className={styles.labels}>Contact number:</p>
+        <p className={styles.information}>+359 87 873 5795</p>
+        <p className={styles.labels}>Contact email:</p>
+        <p className={styles.information}>g.prusiyski@webrika.bg</p>
+      </div>
+      <Button
+      additionalStyles={styles.btn}
+        toggleForm={toggleForm}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
