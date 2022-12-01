d2d5ed60c23ca36290c967c81a48e8cb972c1d43
vladislav99dev <vladislavcho99@abv.bg>
2022-12-01 12:09:20 +0200

    make css responsive to Mobile Screens in Video

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,9 +1,10 @@
 import React from "react";
+import * as styles from "./Video.module.css"
 
 const Video = ({video,children}) => {
   return (
-    <div className='relative h-screen' >
-    <video className="w-full h-full object-fill" autoplay="autoplay" muted loop>
+    <div className={styles.container} >
+    <video className={styles.video} autoplay="autoplay" muted loop>
       <source  src={video} type="video/mp4"/>
     </video>
       {children}
