efa89d92bda806abf532e568eb1e780f6fd60980
vladislav99dev <vladislavcho99@abv.bg>
2022-12-01 16:04:18 +0200

    finish navigation responsivness

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby";
 import Button from "../Button/Button";
 
 import { HamburgerIcon } from "../../assets/images/btnsIcons/btnsIcons";
+import { CloseIconNav } from "../../assets/images/btnsIcons/btnsIcons";
 import { WebrikaMobileLogo } from "../../assets/images/logos/logos";
 import { WebrikaDesktopLogo } from "../../assets/images/logos/logos";
 
@@ -12,7 +13,7 @@ import * as styles from "./Navbar.module.css";
 const Navbar = ({ toggleForm, backgroundColor, width, NavLogo }) => {
   const isBrowser = typeof window !== "undefined";
 
-  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(true);
 
   const toggleNav = () => {
     setIsMobileNavOpen(!isMobileNavOpen);
@@ -25,47 +26,48 @@ const Navbar = ({ toggleForm, backgroundColor, width, NavLogo }) => {
   }
 
   return (
+    <>
     <nav
       className={
         isMobileNavOpen
-          ? `${styles.navigation} ${styles.showNav}`
-          : `${styles.navigation} ${styles.hideNav}`
+        ? `${styles.navigation} ${styles.showMobileNavigation}`
+        : `${styles.navigation} ${styles.hideMobileNavigation}`
       }
-    >
+      >
       <div className={styles.desktopLogo}>
         <WebrikaDesktopLogo />
       </div>
 
-      {/* <div className={styles.mobileLogo}>
-        <WebrikaMobileLogo />
-      </div> */}
+      <div className={styles.mobileIcon} onClick={toggleNav}>
+        {isMobileNavOpen ? <CloseIconNav/> : <HamburgerIcon/>}
+      </div>
 
       <ul
-        className={isMobileNavOpen ? `${styles.mobileList} ${styles.showNavList}` : `${styles.list}`}
-      >
-        <li>
+        className={
+          isMobileNavOpen
+          ? `${styles.links} ${styles.showLinks}`
+          : `${styles.links} ${styles.hideLinks}`
+        }
+        >
+        <li onClick={toggleNav}>
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li onClick={toggleNav}>
           <Link to="/about">About us </Link>
         </li>
         <li onClick={toggleForm}>
           <Link to="#contact">Contact us</Link>
         </li>
-        <li>
+        <li onClick={toggleNav}>
           <Link to="#">Academy</Link>
         </li>
-        <li>
+        <li onClick={toggleNav}>
           <Button additionalStyles={styles.btn} toggleForm={toggleForm} />
         </li>
       </ul>
-      <div className={styles.mobileMenu} onClick={toggleNav}>
-        <HamburgerIcon />
-      </div>
-      {/* <div className={styles.mobileMenu} onClick={toggleNav}>
-        <HamburgerIcon />
-      </div> */}
     </nav>
+    <div className={isMobileNavOpen ? `${styles.placeholder}`: null}></div>
+    </>
   );
 };
 
