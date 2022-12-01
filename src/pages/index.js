766337695bd0c8fee148aab83e49b11dc82e345f
vladislav99dev <vladislavcho99@abv.bg>
2022-12-01 17:52:44 +0200

    add Hero To home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import { useFormToggleContext } from "../context/toggleFormContext";
 
+import Hero from "../components/Hero/Hero";
 import HowWeWork from "../components/HowWeWork/HowWeWork";
 import Video from "../components/Video/Video";
 import ServicesNProducts from "../components/ServicesNProducts/ServicesNProducts";
@@ -10,9 +11,10 @@ import WeAreWebrikaVideo from "../assets/video/WeAreWebrika.mp4"
 import "../styles/global.css";
 
 const IndexPage = () => {
-  const {toggleForm} = useFormToggleContext();
+  const {toggleForm,isFormOpen} = useFormToggleContext();
   return (
     <>
+    <Hero isFormOpen={isFormOpen} toggleForm={toggleForm}/>
       <HowWeWork toggleForm={toggleForm} />
       <Video video={WeAreWebrikaVideo} />
       <ServicesNProducts />
