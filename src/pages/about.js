9bed27e6ab0ef0f7d069c41f3d2c41580a2e0bf9
vladislav99dev <vladislavcho99@abv.bg>
2022-12-01 17:52:32 +0200

    Add Video Component to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,18 @@
 import * as React from "react";
 
+import Navbar from "../components/Navbar/Navbar"
+import AboutContent from "../components/About/AboutContent";
 import OurTeam from "../components/OurTeam/OurTeam";
-
+import Video from "../components/Video/Video";
+import HeroVideo from "../assets/video/heroVideo.mp4";
 import "../styles/global.css";
 
 const About = () => {
+
+
   return (
     <>
+      <Video video={HeroVideo}>{<><Navbar/> <AboutContent/> </>}</Video>
       <OurTeam />
     </>
   );
@@ -15,5 +21,3 @@ const About = () => {
 export default About;
 
 export const Head = () => <title>About Page</title>;
-
-
