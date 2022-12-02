3d74cbdec654b03e26e37d297ee8e782d2447b20
vladislav99dev <vladislavcho99@abv.bg>
2022-12-02 17:40:35 +0200

    add design page

---
+++ src/pages/design.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Design = () => {
+
+    return (
+        <>
+        <h1>Hello</h1>
+        </>
+    );
+  };
+  
+  export default Design;
+  
+  export const Head = () => <title>Design</title>;
