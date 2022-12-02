a1f1822e8ed2db423fff6922fd9826f21b588b12
vladislav99dev <vladislavcho99@abv.bg>
2022-12-02 14:09:04 +0200

    pass dynamic stylings to Navigation

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,27 @@
 import * as React from "react";
 
-import Navbar from "../components/Navbar/Navbar"
+import Navbar from "../components/Navbar/Navbar";
 import AboutContent from "../components/About/AboutContent";
 import OurTeam from "../components/OurTeam/OurTeam";
 import Video from "../components/Video/Video";
 import HeroVideo from "../assets/video/heroVideo.mp4";
+import {WebrikaNavWhiteLogo} from  "../assets/images/logos/logos"
+import {useFormToggleContext} from "../context/toggleFormContext"
+
 import "../styles/global.css";
 
 const About = () => {
-
-
+  const {toggleForm} = useFormToggleContext()
   return (
     <>
-      <Video video={HeroVideo}>{<><Navbar/> <AboutContent/> </>}</Video>
+      <Video video={HeroVideo}>
+        {
+          <>
+            <Navbar NavLogo={WebrikaNavWhiteLogo} toggleForm={toggleForm} additionalStyles="lg:bg-white text-[#3b485e]" />
+            <AboutContent />
+          </>
+        }
+      </Video>
       <OurTeam />
     </>
   );
