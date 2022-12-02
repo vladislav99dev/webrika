ec6b78958b6080f3d4d06ab60e3738b349da66b0
vladislav99dev <vladislavcho99@abv.bg>
2022-12-02 17:40:54 +0200

    change index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import * as React from "react";
 
 import { useFormToggleContext } from "../context/toggleFormContext";
 
-import Hero from "../components/Hero/Hero";
 import HowWeWork from "../components/HowWeWork/HowWeWork";
 import Video from "../components/Video/Video";
 import ServicesNProducts from "../components/ServicesNProducts/ServicesNProducts";
@@ -11,10 +10,9 @@ import WeAreWebrikaVideo from "../assets/video/WeAreWebrika.mp4"
 import "../styles/global.css";
 
 const IndexPage = () => {
-  const {toggleForm,isFormOpen} = useFormToggleContext();
+  const {toggleForm} = useFormToggleContext();
   return (
     <>
-    <Hero isFormOpen={isFormOpen} toggleForm={toggleForm}/>
       <HowWeWork toggleForm={toggleForm} />
       <Video video={WeAreWebrikaVideo} />
       <ServicesNProducts />
