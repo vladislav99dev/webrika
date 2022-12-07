1c72c682e9cdfe92a0e9be0bc2f27c07aa61ea9c
vladislav99dev <vladislavcho99@abv.bg>
2022-12-07 17:43:28 +0200

    add Portfolio to development page

--- src/pages/development.js
+++ src/pages/development.js
@@ -1,12 +1,16 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { dataSelector } from "../assets/data/servicesPageData";
 import ServicesTools from "../components/ServicesTools/ServicesTools";
+import Portfolio from "../components/Portfolio/Portfolio";
 
 const Development = ({ path }) => {
-  const [data,setData] = useState(dataSelector(path));
+  const [data, setData] = useState(dataSelector(path));
 
-
-  return <>{data ? <ServicesTools data={data} path={path} /> : null}</>;
+  return (
+    <>
+      {data ? <ServicesTools data={data} path={path} /> : null} <Portfolio />
+    </>
+  );
 };
 
 export default Development;
