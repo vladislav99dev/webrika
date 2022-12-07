6e53c51824562f1c16252868012d5a2174296893
vladislav99dev <vladislavcho99@abv.bg>
2022-12-07 17:45:14 +0200

    Portfolio component finish

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+
+import ProjectsListings from "../ProjectsListing/ProjectsListings";
 
 import * as styles from "./Portfolio.module.css";
 
 const Portfolio = () => {
+  const [component, setComponent] = useState("siteListings");
+
+  const changeComponent = (value) => {
+    setComponent(value);
+  };
+
   return (
     <section className={styles.container}>
       <h2 className={styles.heading}>Portfolio</h2>
       <div className={styles.tagsContainer}>
-        <p>Development</p>
-        <p>Landing Pages</p>
+        <p onClick={changeComponent.bind(null, "siteListings")}>Development</p>
+        <p onClick={changeComponent.bind(null, "landingPages")}>
+          Landing Pages
+        </p>
       </div>
+      <div className={styles.componentConteiner}>
+      <ProjectsListings/>
+      </div>
+
     </section>
   );
 };
