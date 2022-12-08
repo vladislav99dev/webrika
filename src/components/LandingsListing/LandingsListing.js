12c6da57902d70a4a1df50a1b1fe0f512f4e14e1
vladislav99dev <vladislavcho99@abv.bg>
2022-12-08 16:59:00 +0200

    adding LandingPagesListings

---
+++ src/components/LandingsListing/LandingsListing.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useStaticQuery,graphql,Link } from 'gatsby';
+import { GatsbyImage,getImage } from 'gatsby-plugin-image';
+
+import * as styles from "./LandingsListing.module.css";
+
+
+
+const LandingsListing = () => {
+
+    const {allContentfulWebrikaLandingPages:{nodes:pages}} = useStaticQuery(graphql`
+    query {
+      allContentfulWebrikaLandingPages {
+        nodes {
+          id
+          poster {
+            gatsbyImageData(placeholder: BLURRED)
+          }
+          projectName
+        }
+      }
+    }
+  `)
+
+  console.log(pages);
+  return (
+   <section className={styles.container}>
+    {pages.map((page) => {
+        const imagePath = getImage(page.poster)
+        return (
+            <Link key={page.id} to={`/landing-pages/${page.projectName}`}>
+            <div className={styles.imageContainer} >
+                <GatsbyImage image={imagePath}/>
+            </div>
+            </Link>
+        )
+    })}
+
+   </section>
+  )
+}
+
+export default LandingsListing
