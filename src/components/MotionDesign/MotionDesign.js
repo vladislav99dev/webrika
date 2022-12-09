abba8c6ee3d305864fed17160b1f72fa0bf6c37b
vladislav99dev <vladislavcho99@abv.bg>
2022-12-09 17:36:07 +0200

    add architecture layout for MotionDesign

---
+++ src/components/MotionDesign/MotionDesign.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MotionDesign = () => {
+  return (
+    <div className='flex justify-center'>
+
+    {/* <video className='h-[600px]'  autoPlay="autoplay" muted loop>
+    <source  src="https://videos.ctfassets.net/hm9gh07rmhay/5qYn6Cub9n0KzbMNEDyFnu/617d6174c1a7b514cdf6e34dceb4c7ec/11.mp4" type="video/mp4"/>
+  </video> */}
+    </div>
+  )
+}
+
+export default MotionDesign
