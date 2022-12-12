f3deae92c420c7007c32784cccd2c73411ce5fa4
vladislav99dev <vladislavcho99@abv.bg>
2022-12-12 12:17:25 +0200

    make  Motion Design

--- src/components/MotionDesign/MotionDesign.js
+++ src/components/MotionDesign/MotionDesign.js
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import MotionVideo from "./Video/MotionVideo";
+
+import { useStaticQuery, graphql } from "gatsby";
+
+import * as styles from "./MotionDesign.module.css"
 
 const MotionDesign = () => {
+  const [videoUrl, setVideoUrl] = useState("");
+
+  const videoUrlChanger = (url) => {
+    setVideoUrl(url);
+  };
+
+  const {
+    allContentfulMotionDesign: { nodes: videos },
+  } = useStaticQuery(graphql`
+    query {
+      allContentfulMotionDesign {
+        nodes {
+          id
+          video {
+            url
+          }
+          poster {
+            gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH)
+          }
+        }
+      }
+    }
+  `);
+
   return (
-    <div className='flex justify-center'>
-
-    
+    <div className={styles.container}>
+      {videos.map((video) => {
+        return <MotionVideo
+          key={video.id}
+          videoUrl={video.video.url}
+          videoUrlChanger={videoUrlChanger}
+          posterImage={video.poster}
+        />;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default MotionDesign
+{/* <video className="h-[600px]" autoPlay="autoplay" muted loop>
+<source
+  src={videoUrl}
+  type="video/mp4"
+/>
+</video> */}
+
+export default MotionDesign;
