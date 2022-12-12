5655b45ec32c5a8883bead69d6a9dac9bf55fcab
vladislav99dev <vladislavcho99@abv.bg>
2022-12-12 12:18:47 +0200

    update Portfolio to be dynamic

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 
 import ProjectsListings from "../ProjectsListing/ProjectsListings";
 import LandingsListing from "../LandingsListing/LandingsListing";
@@ -6,48 +6,125 @@ import MotionDesign from "../MotionDesign/MotionDesign";
 
 import * as styles from "./Portfolio.module.css";
 
-const Portfolio = ({ websiteListingName }) => {
-  const [component, setComponent] = useState("siteListings");
+const initialReducerValue = {
+  motionDesign: false,
+  projectsListings: false,
+  landingPages: false,
+  slider: false,
+};
 
-  const changeComponent = (value) => {
-    setComponent(value);
+const portfolioToggler = (state, action) => {
+  switch (action.type) {
+    case "motionDesign":
+      return {
+        motionDesign: !state.motionDesign,
+        projectsListing: false,
+        landingPages: false,
+        slider: false,
+      };
+    case "projectsListings":
+      return {
+        motionDesing: false,
+        projectsListings: !state.projectsListings,
+        landingPages: false,
+        slider: false,
+      };
+    case "landingPages":
+      return {
+        motionDesing: false,
+        projectsListing: false,
+        landingPages: !state.landingPages,
+        slider: false,
+      };
+    case "slider":
+      return {
+        motionDesing: false,
+        projectsListing: false,
+        landingPages: false,
+        slider: !state.slider,
+      };
+  }
+};
+
+const Portfolio = ({ tags }) => {
+  const [state, dispatch] = useReducer(portfolioToggler, initialReducerValue);
+
+  const toggler = (data, event) => {
+    dispatch({ type: data });
+    console.log(state[data]);
   };
-
-  const listingStyles = `${
-    component === "siteListings"
-      ? "border-yellow text-white"
-      : "border-yellow-opacity text-[#D3D3D3]"
-  }`;
-  const landingStyles = `border-b-2 ${
-    component === "landingPages"
-      ? "border-yellow text-white"
-      : "border-yellow-opacity text-[#D3D3D3]"
-  }`;
+  const activeTagStyles = "border-yellow text-white"
+  const unActiveTagStyles = "border-yellow-opacity text-[#D3D3D3]"
 
   return (
     <section className={styles.container}>
       <h2 className={styles.heading}>Portfolio</h2>
       <div className={styles.tagsContainer}>
-        <p
-          className={`border-b-2 cursor-pointer ${listingStyles} `}
-          onClick={changeComponent.bind(null, "siteListings")}
-        >
-          {websiteListingName}
-        </p>
-        <p
-          className={`border-b-2 cursor-pointer ${landingStyles} `}
-          onClick={changeComponent.bind(null, "landingPages")}
-        >
-          Landing Pages
-        </p>
+        {tags.map((tag) => {
+          const {data,text} = tag
+          return (
+            <p
+              className={`border-b-2 cursor-pointer ${
+                state[data]
+                  ? activeTagStyles
+                  : unActiveTagStyles
+              }`}
+              onClick={toggler.bind(null, data)}
+            >
+              {text}
+            </p>
+          );
+        })}
       </div>
       <div className={styles.componentConteiner}>
-        {component === "siteListings" ? <ProjectsListings /> : null}
-        {component === "landingPages" ? <LandingsListing /> : null}
+        {state.projectsListings ? <ProjectsListings /> : null}
+        {state.landingPages ? <LandingsListing /> : null}
+        {state.motionDesign ? <MotionDesign /> : null}
       </div>
-      <MotionDesign />
     </section>
   );
 };
 
 export default Portfolio;
+//   const [component, setComponent] = useState("siteListings");
+
+//   const changeComponent = (value) => {
+//     setComponent(value);
+//   };
+
+//   const listingStyles = `${
+//     component === "siteListings"
+//       ? "border-yellow text-white"
+//       : "border-yellow-opacity text-[#D3D3D3]"
+//   }`;
+//   const landingStyles = `border-b-2 ${
+//     component === "landingPages"
+//       ? "border-yellow text-white"
+//       : "border-yellow-opacity text-[#D3D3D3]"
+//   }`;
+
+//   return (
+//     <section className={styles.container}>
+//       <h2 className={styles.heading}>Portfolio</h2>
+//       <div className={styles.tagsContainer}>
+//         <p
+//           className={`border-b-2 cursor-pointer ${listingStyles} `}
+//           onClick={changeComponent.bind(null, "siteListings")}
+//         >
+//           {websiteListingName}
+//         </p>
+//         <p
+//           className={`border-b-2 cursor-pointer ${landingStyles} `}
+//           onClick={changeComponent.bind(null, "landingPages")}
+//         >
+//           Landing Pages
+//         </p>
+//       </div>
+//       <div className={styles.componentConteiner}>
+//         {component === "siteListings" ? <ProjectsListings /> : null}
+//         {component === "landingPages" ? <LandingsListing /> : null}
+//       </div>
+//       <MotionDesign />
+//     </section>
+//   );
+// };
